5b378e5cec18c6fe4ee99818e3c2db7c9dddc7f1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-03 07:37:06 -0500

    add more tests

--- cypress/e2e/store-email.cy.js
+++ cypress/e2e/store-email.cy.js
@@ -43,8 +43,17 @@ describe('An email', () => {
     })
   })
 
-  it.only('has the confirmation code', function () {
+  beforeEach(function () {
     cy.document({ log: false }).invoke({ log: false }, 'write', this.email)
+  })
+
+  it('shows the code by itself', () => {
+    cy.contains('strong', /^[a-z0-9]{8}$/)
+      .invoke('text')
+      .log()
+  })
+
+  it('has the confirmation code link', function () {
     cy.contains('a', 'Enter the confirmation code')
       .should('be.visible')
       .as('codeLink')
@@ -58,48 +67,26 @@ describe('An email', () => {
       })
   })
 
-  it('has the confirmation code 2', () => {
-    cy.log('**email has the user name**')
-    cy.contains(`Dear ${userName},`).should('be.visible')
-    cy.log('**email has the confirmation code**')
+  it('has the working code', function () {
     cy.contains('a', 'Enter the confirmation code')
       .should('be.visible')
-      .as('codeLink')
-      .invoke('text')
-      .then((text) => Cypress._.last(text.split(' ')))
-      .then((code) => {
-        cy.log(`**confirm the code ${code} works**`)
-        expect(code, 'confirmation code')
-          .to.be.a('string')
-          .and.have.length.gt(5)
-
-        // add synthetic delay, otherwise the email
-        // flashes very quickly
-        cy.wait(2000)
-
-        // before we click on the link, let's make sure it
-        // does not open a new browser window
-        // https://glebbahmutov.com/blog/cypress-second-tab/
-        cy.get('@codeLink')
-          // by default the link wants to open a new window
-          .should('have.attr', 'target', '_blank')
-          // but the test can point the open back at itself
-          // so the click opens it in the current browser window
-          .invoke('attr', 'target', '_self')
-          .click()
-
-        // confirm the URL changed back to our web app
-        cy.location('pathname', { timeout: 30000 }).should('equal', '/confirm')
-        cy.location('search').should('equal', `?code=${code}`)
-        cy.log(`input code field is prefilled with code ${code}`)
-        cy.get('#confirmation_code')
-          .should('be.visible')
-          .and('have.value', code)
-        cy.get('button[type=submit]').click()
-        // first positive assertion, then negative
-        // https://glebbahmutov.com/blog/negative-assertions/
-        cy.get('[data-cy=confirmed-code]').should('be.visible')
-        cy.get('[data-cy=incorrect-code]').should('not.exist')
-      })
+      // by default the link wants to open a new window
+      .and('have.attr', 'target', '_blank')
+      // but the test can point the open back at itself
+      // so the click opens it in the current browser window
+      .invoke('attr', 'target', '_self')
+      .click()
+    // confirm the URL changed back to our web app
+    cy.location('pathname', { timeout: 30000 }).should('equal', '/confirm')
+    cy.location('search').should('include', `?code=`)
+    cy.get('#confirmation_code')
+      .should('be.visible')
+      .invoke('val')
+      .should('not.be.empty')
+    cy.get('button[type=submit]').click()
+    // first positive assertion, then negative
+    // https://glebbahmutov.com/blog/negative-assertions/
+    cy.get('[data-cy=confirmed-code]').should('be.visible')
+    cy.get('[data-cy=incorrect-code]').should('not.exist')
   })
 })
